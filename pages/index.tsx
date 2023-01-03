ad98366e3eb09b2461ee9696c03a3b33efd788f9
Nils Goksör <nils.goksor@gmail.com>
2023-01-03 11:39:50 +0100

    brushshart example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
 import Head from "next/head";
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("../components/BrushChart"), { ssr: false });
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
     <>
       <Head>
@@ -19,8 +30,13 @@ export default function Home() {
           </div>
           <div>all</div>
         </div>
-        <div>
+        <div style={{ height: 500 }}>
           <h2>Graph</h2>
+          {!loading && (
+            <ParentSize>
+              {({ width, height }) => <Chart width={width} height={height} />}
+            </ParentSize>
+          )}
         </div>
       </main>
     </>
