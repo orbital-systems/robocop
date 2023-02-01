c8e529abd2db62422cdd8ab6e70f2250bab0742d
Nils Goksör <nils.goksor@gmail.com>
2023-02-01 16:34:14 +0100

    use join data to show relationship diagnose/symptoms

--- pages/diagnoses/chart.tsx
+++ pages/diagnoses/chart.tsx
@@ -1,8 +1,14 @@
 import dynamic from "next/dynamic";
 import ParentSize from "@visx/responsive/lib/components/ParentSize";
-import { Diagnosis, DateInterval } from "../../types";
-import { useState } from "react";
-import { getDiagnosisColor, getDiagnosisName } from "../../util";
+import { Diagnosis, DateInterval, Symptom } from "../../types";
+import { useEffect, useState } from "react";
+import {
+  getDiagnosisColor,
+  getDiagnosisName,
+  getSymptomColor,
+  getSymptomName,
+} from "../../util";
+import { joins, symptoms } from "../../exampledata";
 
 const AreaChart = dynamic(() => import("../../components/Chart"), {
   ssr: false,
@@ -29,6 +35,12 @@ export const Chart = ({
   setFilteredData,
   externalFilter,
 }: ChartProps) => {
+  const [selectedDiagnose, setSelectedDiagnose] = useState<
+    Diagnosis | undefined
+  >(undefined);
+
+  const [triggeredBy, setTriggeredBy] = useState<Symptom[]>([]);
+
   const [valueAccessor, setValueAccessor] = useState<"installation" | "code">(
     "installation"
   );
@@ -41,6 +53,19 @@ export const Chart = ({
 
   const getValueAccessor =
     valueAccessor === "code" ? codeAccessor : installationAccessor;
+
+  useEffect(() => {
+    if (typeof selectedDiagnose !== "undefined") {
+      const triggeredByList =
+        joins
+          ?.filter((j) => j.diagnosis_id === selectedDiagnose?.diagnosis_id)
+          ?.map((d) => symptoms.find((s) => s.symptom_id === d.symptom_id))
+          ?.filter((s) => typeof s !== "undefined") || [];
+
+      const temporaryUglyCode = triggeredByList as unknown as Symptom[]; // this is temporary anyway :)
+      setTriggeredBy(temporaryUglyCode);
+    }
+  }, [selectedDiagnose]);
 
   return (
     <>
@@ -63,6 +88,9 @@ export const Chart = ({
                 dateInterval={dateInterval}
                 setDateInterval={setDateInterval}
                 getCodeColor={getDiagnosisColor}
+                onDataPointClick={(point: Diagnosis) =>
+                  setSelectedDiagnose(point)
+                }
                 hoverContent={(dataPoint) => (
                   <div style={{ display: "flex" }}>
                     <div
@@ -78,6 +106,74 @@ export const Chart = ({
               />
             )}
           </ParentSize>
+        )}
+      </div>
+      <div>
+        {typeof selectedDiagnose !== "undefined" ? (
+          <div
+            style={{
+              backgroundImage:
+                "linear-gradient(to right, #0a325733, #04713080)",
+              padding: 15,
+              borderRadius: 8,
+            }}
+          >
+            <div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <h3>{`${selectedDiagnose.code} - ${getDiagnosisName(
+                  selectedDiagnose.code
+                )}`}</h3>
+                <button onClick={() => setSelectedDiagnose(undefined)}>
+                  close
+                </button>
+              </div>
+              <span>
+                {`${new Date(selectedDiagnose.timestamp).toLocaleString()} on `}
+                <a
+                  href={`https://osw.orb-sys.com/device/?id=${selectedDiagnose.device_id}`}
+                >
+                  device
+                </a>
+              </span>
+            </div>
+            <h4>Triggered by:</h4>
+            <ul>
+              {triggeredBy?.map((t) => (
+                <li key={t.symptom_id}>
+                  <div
+                    style={{
+                      backgroundColor: getSymptomColor(t.code),
+                      padding: 8,
+                    }}
+                  >
+                    <p>{`${t.code} - ${getSymptomName(t.code)}`}</p>
+                    <span>
+                      {`${new Date(t.timestamp).toLocaleString()} on `}
+                      <a
+                        href={`https://osw.orb-sys.com/device/?id=${t.device_id}`}
+                      >
+                        device
+                      </a>
+                    </span>
+                    {" : "}
+                    <a
+                      href={`https://osw.orb-sys.com/plotting/?device_id=${t.device_id}&session_id=${t.session_id}`}
+                    >
+                      session
+                    </a>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>Click on a point to get more information about the diagnosis</p>
         )}
       </div>
     </>
